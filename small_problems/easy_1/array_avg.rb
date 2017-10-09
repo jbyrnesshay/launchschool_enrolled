77fe603824b9395c5e17259d2b446f07940a2653
jbyrnesshay <jbyrnes.shay@gmail.com>
2017-10-09 06:42:28 -0400

    some small problems work

--- small_problems/easy_1/array_avg.rb
+++ small_problems/easy_1/array_avg.rb
@@ -8,3 +8,11 @@ end
 
 puts average([1, 5, 87, 45, 8, 3]) 
 puts average([9, 47, 23, 95, 16, 52]) 
+
+# or 
+
+def average2(array)
+  array.reduce(:+).to_f / array.length
+end
+
+puts average2([1, 5, 87, 45, 8, 6])
--- small_problems/easy_1/sum_of_dig.rb
+++ small_problems/easy_1/sum_of_dig.rb
@@ -9,4 +9,38 @@ def sum(num)
   add
 end
 
-puts sum(123_456_789)
+test = 123_456_789
+puts sum(test)
+
+
+#or
+
+def sum2(num)
+  numbers = num.to_s.scan(/\d/)
+  sum = 0
+  numbers.each do |item|
+    sum += item.to_i
+  end
+  sum
+end
+
+puts sum2(test)
+
+#or
+
+def sum3(num)
+  num.to_s.chars.map(&:to_i).reduce(:+)
+end
+
+puts sum3(test)
+
+
+def thing(thing)
+  if thing.is_a? String
+    thing = thing.split()
+  end
+  thing.reduce(:+)
+end
+
+
+puts thing("I dont know")
