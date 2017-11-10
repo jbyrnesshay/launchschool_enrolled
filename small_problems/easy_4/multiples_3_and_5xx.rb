1b5099cab380f109cb5cf1ba8e163a420e44941a
jbyrnesshay <jbyrnes.shay@gmail.com>
2017-11-10 03:10:57 -0500

    tictactoe player turn

--- small_problems/easy_4/multiples_3_and_5xx.rb
+++ small_problems/easy_4/multiples_3_and_5xx.rb
@@ -1,10 +1,10 @@
 
 
 def within_range?(num, limit)
-  num <= limit
+  (1..limit).include? num
 end
 
-def add_eligible_multiple(number, array)
+def add_unique_multiple(number, array)
   if (!array.include? number) 
     array << number
   end
@@ -15,7 +15,7 @@ def create_multiples(list, limit)
   (1..limit).each do |number|
     list.each do |multiplier|
       multiple = multiplier * number
-      within_range?(multiple, limit) ? add_eligible_multiple(multiple, array): nil
+      within_range?(multiple, limit) ? add_unique_multiple(multiple, array): nil
     end
   end
   array.sort
@@ -32,7 +32,6 @@ end
 # user configurable values (list is a list of multipliers)
 list = [3,5]
 limit = 1000
+options = {:limit => limit, :multiples => list}
 
-parameters = {:limit => limit, :multiples => list}
-
-p multisum(parameters)
+p multisum(options)
