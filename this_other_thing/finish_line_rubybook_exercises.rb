77f6ab67ecd9155b3eb952d52f6eb14dec47e2ed
jbyrnesshay <jbyrnes.shay@gmail.com>
2017-11-30 06:21:20 -0500

    added a few

--- this_other_thing/finish_line_rubybook_exercises.rb
+++ this_other_thing/finish_line_rubybook_exercises.rb
@@ -96,3 +96,64 @@ a.map! do |l|
 end
 a.flatten!
 p a
+
+# extra
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+attributes = [:email, :address, :phone]
+contacts.each do |contact, values|
+  attributes.each do |attribute|
+    values[attribute] = contact_data.first.first
+    contact_data.first.shift
+  end
+  contact_data.shift
+end
+puts 'this thing here'
+p contacts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
