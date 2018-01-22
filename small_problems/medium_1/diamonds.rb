d2fd9c11d50c572189243491f6c54d2d9ba35f2b
jbyrnesshay <jbyrnes.shay@gmail.com>
2018-01-22 04:07:32 -0500

    just making sure

--- small_problems/medium_1/diamonds.rb
+++ small_problems/medium_1/diamonds.rb
@@ -4,6 +4,7 @@
 
 #working method
 def diamond(num)
+  #displayes a num*num sized grid in shape of diamondm where num is odd integer
   start = num / 2
   num.times do |x|
     puts (' ' * start.abs) + '*' * (num - 2*start.abs)
@@ -17,6 +18,7 @@ diamond(19)
 diamond(7)
 diamond(3)
 diamond(25)
+diamond(21)
 
 #try an alternative method
 
